12bb73a4825dc0ee289b2a317bbe9323713ba076
muhammadderic <muhammad.donny.e@gmail.com>
2023-06-07 12:01:51 +0700

    Create mobile responsive - Navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,47 @@ import CartBasket from "./CartBasket";
 export default function Navbar() {
   const [isOpenCard, setIsOpenCart] = useState(false);
 
-
   const openCartModal = () => {
     setIsOpenCart(!isOpenCard);
+  }
+
+  const openMenu = () => {
+    const menu = document.querySelector(".menu");
+    const closeIcon = document.querySelector(".closeIcon");
+    const menuIcon = document.querySelector(".menuIcon");
+
+    if (menu.classList.contains("showMenu")) {
+      menu.classList.remove("showMenu");
+      closeIcon.style.display = "none";
+      menuIcon.style.display = "block";
+    } else {
+      menu.classList.add("showMenu");
+      closeIcon.style.display = "block";
+      menuIcon.style.display = "none";
+    }
   }
 
   return (
     <div className="container">
       <div className="navbar wrapper">
         <div className="navbar__left">
+          <button className="hamburger" onClick={openMenu}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="menuIcon" width="70"
+              height="70" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round"
+              strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M4 6l16 0"></path>
+              <path d="M4 12l16 0"></path>
+              <path d="M4 18l16 0"></path>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" className="closeIcon" width="70" height="70"
+              viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round"
+              strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M18 6l-12 12"></path>
+              <path d="M6 6l12 12"></path>
+            </svg>
+          </button>
           <img src="/images/logo.svg" alt="company logo" className="navbar__logo" />
           <ul className="menu">
             <li className="menu__item">
